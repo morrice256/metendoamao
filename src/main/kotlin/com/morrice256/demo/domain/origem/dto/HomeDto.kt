63fd67699aacfcf146d6172d05ab3c31dfc2bed0
Mauricio W Martins <mauriciowanderleymartins@gmail.com>
2023-07-15 09:13:56 -0300

    Ajuste final

--- src/main/kotlin/com/morrice256/demo/domain/origem/dto/HomeDto.kt
+++
@@ -1,8 +0,0 @@
-package com.morrice256.demo.domain.origem.dto
-
-class HomeDto(
-        var identifier: Long,
-        var name: String,
-        var city: String,
-        var state: String,
-        var accountableDocument: String)
--- src/main/kotlin/com/morrice256/demo/domain/pessoa/PessoaJuridicaRepository.kt
+++
@@ -1,6 +0,0 @@
-package com.morrice256.demo.domain.pessoa
-
-import org.springframework.data.repository.CrudRepository
-
-interface PessoaJuridicaRepository: CrudRepository<PessoaJuridica, Long>{
-}

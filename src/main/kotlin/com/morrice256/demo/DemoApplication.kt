745b4a30fa27e6260669d1e8ef3ac37cf5be2eec
Mauricio W Martins <mauriciowanderleymartins@gmail.com>
2023-08-02 21:15:44 -0300

    Add properties

--- src/main/kotlin/com/morrice256/demo/DemoApplication.kt
+++ src/main/kotlin/com/morrice256/demo/DemoApplication.kt
@@ -1,5 +1,7 @@
 package com.morrice256.demo
 
+import jakarta.annotation.PostConstruct
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.autoconfigure.domain.EntityScan
 import org.springframework.boot.runApplication
@@ -10,7 +12,23 @@ import org.springframework.boot.runApplication
 	"com.morrice256.demo.domain.*",
 	"com.morrice256.demo.service.*"
 )
-class DemoApplication
+class DemoApplication {
+
+	@Value("\${spring.profiles.active}")
+	var profileActive: String = ""
+
+	@Value("\${demo.value}")
+	var demoValue: String = ""
+
+	@PostConstruct
+	fun showProfileActive() {
+		println("================================")
+		println(" ")
+		println("Profile Active: ${profileActive} - ${demoValue}" )
+		println(" ")
+		println("================================")
+	}
+}
 
 fun main(args: Array<String>) {
 	runApplication<DemoApplication>(*args)
